d848bd8196885ab7889b3b641ca04b8725b523c3
eduardorgv <eduardsabb@outlook.com>
2024-05-14 19:59:45 -0600

    fix: reset form

--- src/app/components/compose-post.tsx
+++ src/app/components/compose-post.tsx
@@ -4,6 +4,7 @@ import { Avatar } from "@nextui-org/react";
 import { createPost } from "../actions/add-post-action";
 import { SubmitButton } from "./compose-post-submit-button";
 import { Textarea } from "@nextui-org/input";
+import { useRef } from "react";
 
 interface iProps {
   userName: string;
@@ -11,9 +12,15 @@ interface iProps {
 }
 
 export function ComposePost({ userName, userAvatarUrl }: iProps) {
+  const ref = useRef<HTMLFormElement>(null);
+
   return (
     <form
-      action={createPost}
+      ref={ref}
+      action={async (formData) => {
+        await createPost(formData);
+        ref.current?.reset();
+      }}
       className="flex flex-row p-4 border-b border-white/20 gap-3"
     >
       <Avatar radius="full" size="md" src={userAvatarUrl} alt={userName} />
